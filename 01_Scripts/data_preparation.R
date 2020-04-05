ab2052db95987311f77c361efb36fea8ee3f8077
Jordan Cantet <jordan.cantet@gmail.com>
2020-04-05 12:55:22 +0200

    Utilisation du package xml2

--- 01_Scripts/data_preparation.R
+++ 01_Scripts/data_preparation.R
@@ -19,8 +19,19 @@ rm(package,packages)
 
 # Mise en forme des données
   # Traitement du fichier XML source en dataframe
-  Prix_20200403 <- read_xml("00_Inputs/PrixCarburants_quotidien_20200403.xml")
-  Prix_list <- as_list(Prix_20200403)
-  Prix_df <- data.frame(Reduce(rbind, Prix_R))
-  rm(Prix_R)
+  prix_20200403 <- read_xml("00_Inputs/PrixCarburants_quotidien_20200403.xml")
+  prix_list <- as_list(prix_20200403)
+  prix_df <- as_tibble(prix_list)
+  prix_df_clean <- unnest_wider(prix_df,col = "pdv_liste")
+  
+  # Matrice avec des listes imbriquées, mais impossible d'aller plus loin pour en faire un dataframe...
+  prix_df_clean2 <- do.call(rbind,prix_list)
+  
+  
+  # 2e essai : utilisation du package xml2
+  name <- xml_name(prix_20200403)
+  child <- xml_child(prix_20200403)
+  children <- xml_children(prix_20200403)
+  text <- xml_text(prix_20200403) # Pas bon
+  all <- xml_find_all("/adresse/*") # Ne fonctionne pas
   
